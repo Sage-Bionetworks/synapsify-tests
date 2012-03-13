478c491072fe746730540fd39ec1173c1b05d45b
erichhuang <erich.huang@sagebase.org>
2012-03-13 12:30:55 -0700

    Creating a vignette for demonstrating SSS

---
+++ sssVignette/sssVignette.R
@@ -0,0 +1,49 @@
+# sssVignette.R
+
+# Erich S. Huang
+# Sage Bionetworks
+# Seattle, Washington
+# erich.huang@sagebase.org
+
+# This is a vignette for learning how to use our R package implementation of
+# 'Shotgun Stochastic Search' by Chris Hans, Quanli Wang, and Mike West. The 
+# package is authored by Brian Bot of Sage Bionetworks and conveniently wraps
+# and integrates SSS with R
+
+require(Biobase)
+require(factDesign) # A Bioconductor package
+require(sss)
+
+## Bring in the 'Estrogen' dataset from the 'factDesign' package
+data(estrogen)
+exDesign <- pData(estrogen)
+
+# ES TIME
+# et1.CEL  A  10h
+# et2.CEL  A  10h
+# Et1.CEL  P  10h
+# Et2.CEL  P  10h
+# eT1.CEL  A  48h
+# eT2.CEL  A  48h
+# ET1.CEL  P  48h
+# ET2.CEL  P  48h
+
+# This sample dataset comprises estrogen receptor-positive cell lines that are 
+# exposed to estrogen (versus control) over two timepoints. It is a restricted 
+# 500 probe features pulled from Affymetrix HGU95av2 arrays for the above
+# samples
+
+## Let's create a simple SSS logistic regression model for estrogen exposure
+exEstrogen <- exprs(estrogen)
+
+## Let's build a model on the 10h samples and validate on the 48h samples
+trainSet <- t(exEstrogen[ , 1:4]) # Note that the matrix is transposed
+validSet <- t(exEstrogen[ , 5:8])
+trainPheno <- ifelse(exDesign$ES[1:4] == "P", 1, 0)
+validPheno <- ifelse(exDesign$ES[5:8] == "p", 1, 0)
+
+# Fit the model
+sssFit <- sss(trainPheno ~ trainSet)
+
+## Let's look at the features and their posterior marginal probabilities
+sPMP <- sort(sssFit@postMargProb, decreasing = TRUE)
