ee0f0d9d1bfa84f724adff2fc91a7f4bb1eacf1f
erichhuang <erich.huang@sagebase.org>
2012-03-13 14:34:45 -0700

    1st draft of the vignette

--- sssVignette/sssVignette.R
+++ sssVignette/sssVignette.R
@@ -13,6 +13,7 @@
 require(Biobase)
 require(factDesign) # A Bioconductor package
 require(sss)
+require(hgu95av2.db)
 
 ## Bring in the 'Estrogen' dataset from the 'factDesign' package
 data(estrogen)
@@ -37,13 +38,31 @@ exDesign <- pData(estrogen)
 exEstrogen <- exprs(estrogen)
 
 ## Let's build a model on the 10h samples and validate on the 48h samples
-trainSet <- t(exEstrogen[ , 1:4]) # Note that the matrix is transposed
-validSet <- t(exEstrogen[ , 5:8])
-trainPheno <- ifelse(exDesign$ES[1:4] == "P", 1, 0)
-validPheno <- ifelse(exDesign$ES[5:8] == "p", 1, 0)
+validSet <- t(exEstrogen[ , 1:4]) # Note that the matrix is transposed
+trainSet <- t(exEstrogen[ , 5:8])
+validPheno <- ifelse(exDesign$ES[1:4] == "P", 1, 0)
+trainPheno <- ifelse(exDesign$ES[5:8] == "P", 1, 0)
 
 # Fit the model
-sssFit <- sss(trainPheno ~ trainSet)
+sssFit <- sss(trainPheno ~ trainSet, iters = 5000, nbest = 200)
+
+# Run the training set back through the model object
+trainPhenoHat <- predict(sssFit, newdata = trainSet)
+
+# Look at the correlation of 'trainPheno' and 'trainPhenoHat'
+trainRho <- cor(trainPheno, trainPhenoHat)
+# > trainRho^2 = 0.9928368
+
+# Look at the validation set
+validPhenoHat <- predict(sssFit, newdata = validSet)
+
+# Look at the correlation of 'validPheno' and 'validPhenoHat'
+validRho <- cor(validPheno, validPhenoHat)
+# > validRho^2 = 0.9664588
 
 ## Let's look at the features and their posterior marginal probabilities
 sPMP <- sort(sssFit@postMargProb, decreasing = TRUE)
+
+## MAPPING ILLUMINA FEATURES TO GENE SYMBOLS
+topEstrogenSYMs <- as.character(mget(names(sPMP[1:10]), hgu95av2SYMBOL, 
+                              ifnotfound = NA))
